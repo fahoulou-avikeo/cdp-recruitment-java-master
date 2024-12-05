32c3c028ca63afd5c584307d7dcdf56d96ecf1e9
fahoulou <fahoulou@reversys.fr>
2024-12-05 12:07:40 +0100

    Comment

--- src/main/java/adeo/leroymerlin/cdp/helper/BandFormattingHelper.java
+++ src/main/java/adeo/leroymerlin/cdp/helper/BandFormattingHelper.java
@@ -4,9 +4,6 @@ import adeo.leroymerlin.cdp.entity.Band;
 import org.springframework.stereotype.Component;
 
 @Component
-/**
- * Helper
- */
 public class BandFormattingHelper {
 
     public void formatBand(Band band) {
